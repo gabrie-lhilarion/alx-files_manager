0cefd30d11eff2c81b606d132a98e0d5d3fd4b4e
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-09-02 09:20:21 +0100

    Create a new user

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const routes = require('./routes/index');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
 // Load all routes
 app.use('/', routes);
 
