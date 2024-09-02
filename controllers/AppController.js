2c0196524ea3a1c0d1b6856999ce3d002a896e9b
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-09-02 08:19:03 +0100

    First API

---
+++ controllers/AppController.js
@@ -0,0 +1,20 @@
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+class AppController {
+  static async getStatus(req, res) {
+    const redisAlive = redisClient.isAlive();
+    const dbAlive = dbClient.isAlive();
+
+    return res.status(200).json({ redis: redisAlive, db: dbAlive });
+  }
+
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+
+    return res.status(200).json({ users, files });
+  }
+}
+
+module.exports = AppController;
